85590848214f426e4d5c13a4e6044f248d9a6e2f
DOMI <YoumiHa@dingtalk.com>
2026-03-28 15:10:55 +0800

    Update McbeCommandRequest.cs

--- Protocol/Network/MinecraftPacket/McbeCommandRequest.cs
+++ Protocol/Network/MinecraftPacket/McbeCommandRequest.cs
@@ -1,15 +1,14 @@
-﻿using Protocol.Utils;
+﻿using Protocol.Minecraft;
+using Protocol.Utils;
 
 namespace Protocol.Network.MinecraftPacket;
 public class McbeCommandRequest : Packet
 {
-    public string command;
-    public uint commandType;
-    public bool isinternal;
-    public string requestId;
-    public UUID unknownUuid;
-    public int version;
-    public McbeCommandRequest()
+	public string CommandLine { get; set; }
+	public CommandOrigin CommandOrigin { get; set; }
+	public bool Internal { get; set; }
+	public string Version { get; set; }
+	public McbeCommandRequest()
     {
         Id = 0x4d;
         IsMcbe = true;
@@ -18,22 +17,19 @@ public class McbeCommandRequest : Packet
     protected override void EncodePacket()
     {
         base.EncodePacket();
-        Write(command);
-        WriteUnsignedVarInt(commandType);
-        Write(unknownUuid);
-        Write(requestId);
-        Write(isinternal);
-        WriteSignedVarInt(version);
-    }
+		Write(CommandLine);
+		Write(CommandOrigin);
+		Write(Internal);
+		Write(Version);
+	}
 
     protected override void DecodePacket()
     {
         base.DecodePacket();
-        command = ReadString();
-        commandType = ReadUnsignedVarInt();
-        unknownUuid = ReadUUID();
-        requestId = ReadString();
-        isinternal = ReadBool();
-        version = ReadSignedVarInt();
+        CommandLine = ReadString();
+        CommandOrigin = ReadCommandOrigin();
+        Internal = ReadBool();
+        Version = ReadString();
+
     }
 }
