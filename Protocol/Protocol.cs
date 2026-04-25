27dcbcd5b887618758d300fc949d27d0922e2586
DOMI <YoumiHa@dingtalk.com>
2026-04-25 19:01:29 +0800

    Update Protocol.cs

--- Protocol/Protocol.cs
+++ Protocol/Protocol.cs
@@ -4,6 +4,6 @@ using System;
 
 public static class ProtocolInfo
 {
-	public static int McProtocol = 944;
-	public static Version McVersion = Version.Parse("1.26.10");
+	public static int McProtocol = 975;
+	public static Version McVersion = Version.Parse("1.26.20");
 }
